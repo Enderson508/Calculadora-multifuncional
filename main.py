715b724e2c3df23568062cd6af220848807cc2a4
Enderson Augusto Vieira de Lima <endersonvieira409@gmail.com>
2025-05-13 16:49:28 -0300

    main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import uuid
 import bcrypt
 import os
 
-# Carrega ou cria arquivo de dadoss
+# Carrega ou cria arquivo de dados
 if not os.path.exists("users.json"):
     with open("users.json", "w") as f:
         json.dump({}, f)
